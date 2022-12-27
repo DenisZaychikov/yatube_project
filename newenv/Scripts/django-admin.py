e4f4fcacf8af8a60cd2741c62a93bd2e25781df6
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2022-12-28 01:03:19 +0300

    Delete newenv

--- newenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\YandexPracticum\Sprint3\Theory\yatube_project\newenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
